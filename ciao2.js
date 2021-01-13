0a4d6c5a7662c4bf74684b6302cc2f098c72a9b7
enrico alterani <e.alterani@gmail.com>
2021-01-14 00:10:54 +0100

    aggiunto file ciao2-js

---
+++ ciao2.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    
+    var elementoH1 = document.getElementById('idTitoloPagina');
+    var elementoH3 = document.getElementById('idSottotitolo');
+
+
+    elementoH1.innerHTML = "Ciao Mondo! <br> sto eseguendo codice js." ;
+    elementoH3.innerHTML = "il file ciao.js funziona correttamente." ;
+    console.log("sono entrato nell'evento...");
+}); // fine document.addEventListener('DOMContentLoaded'....
+
+//alert("ciao mondo!");
+//ciao da enrico
